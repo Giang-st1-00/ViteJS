ec2091d18eb4668ceedf5ec56d16b70ff0a712ba
Giang ca <bin17022003@gmail.com>
2023-02-08 13:16:33 +0700

    Phân tích thuật toán và code chức năng phân trang

--- src/pages/ProductList/ProductList.tsx
+++ src/pages/ProductList/ProductList.tsx
@@ -1,5 +1,7 @@
 import { useQuery } from '@tanstack/react-query'
+import { useState } from 'react'
 import productApi from 'src/apis/product.api'
+import Pagination from 'src/Components/Pagination'
 import useQueryParams from 'src/hooks/useQueryParams'
 import AsideFilter from './AsideFilter'
 import Product from './Product/Product'
@@ -7,6 +9,7 @@ import SortProductList from './SortProductList'
 
 export default function ProductList() {
   const queryParams = useQueryParams()
+  const [page, setPage] = useState(1)
   const { data } = useQuery({
     queryKey: ['products', queryParams],
     queryFn: () => {
@@ -32,6 +35,7 @@ export default function ProductList() {
                   </div>
                 ))}
             </div>
+            <Pagination page={page} setPage={setPage} pageSize={7} />
           </div>
         </div>
       </div>
