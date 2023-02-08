f19e71ddb9c9b3a08938f9fed8c7bfb3953e4772
Giang ca <bin17022003@gmail.com>
2023-02-08 10:29:23 +0700

    Hiển thị product và format number với Intl

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -10,14 +10,16 @@ export function isAxiosUnprocessableEntityError<FormError>(error: unknown): erro
   return isAxiosError(error) && error.response?.status === HttpStatusCode.UnprocessableEntity
 }
 
+export function formatCurrency(currency: number) {
+  return new Intl.NumberFormat('de-DE').format(currency)
+}
 
-
-
-
-
-
-
-
-
-
-
+export function formatNumberToSocialStyle(value: number) {
+  return new Intl.NumberFormat('en', {
+    notation: 'compact',
+    maximumFractionDigits: 1
+  })
+    .format(value)
+    .replace('.', ',')
+    .toLowerCase()
+}
